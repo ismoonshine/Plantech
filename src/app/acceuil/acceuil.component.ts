988510a213538fb4f42cac58daa59351618f413a
Israa <israamhamdi04@gmail.com>
2024-04-18 22:20:05 +0100

    some fixes

--- src/app/acceuil/acceuil.component.ts
+++ src/app/acceuil/acceuil.component.ts
@@ -1,7 +1,12 @@
 import { Component ,OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { routes } from '../app.routes';
+import { Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+
+export const routes: Routes =[
+  {'path':"register", component: RegisterComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,16 @@ import { RegisterComponent } from './register/register.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AcceuilComponent } from './acceuil/acceuil.component';
-
+import { ProfileComponent } from './profile/profile.component';
+import { PropComponent } from './prop/prop.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent, HttpClientModule, HeaderComponent, AcceuilComponent],
+  imports: [RouterOutlet, RegisterComponent, HttpClientModule, HeaderComponent, AcceuilComponent, ProfileComponent,
+PropComponent, AuthentificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
