703c70d9e067a3732378100ec27c8747214e6530
RachaKhamassi <rachaakhamassi@gmail.com>
2024-04-29 16:53:17 +0100

    page profil

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,3 +12,33 @@ export class ProfileComponent implements OnInit{
   ngOnInit(): void {
   }
 }
+
+fetch('Etudiant.json')
+    .then((response: Response) => response.json())
+    .then((data: any[]) => {
+        if (data && data.length > 0) {
+            let table: string = '<table border="2">';
+            table += '<tr><th>CIN</th><th>Nom</th><th>Prénom</th><th>Date de naissance</th><th>Clubs</th><th>Redoublant</th><th>Moyenne</th></tr>';
+            data.forEach((student: any) => {
+                table += '<tr>';
+                table += '<td>' + student.cin + '</td>';
+                table += '<td>' + student.nom + '</td>';
+                table += '<td>' + student.prenom + '</td>';
+                table += '<td>' + student.date_naissance + '</td>';
+                table += '<td>' + student.clubs.join(', ') + '</td>';
+                table += '<td>' + (student.redoublant ? "Oui" : "Non") + '</td>';
+                table += '<td>' + student.moyenne + '</td>';
+                table += '</tr>';
+            });
+            table += '</table>';
+            const tableElement: HTMLElement | null = document.getElementById("tableetudiant");
+            if (tableElement) {
+                tableElement.innerHTML = table;
+            }
+        } else {
+            const tableElement: HTMLElement | null = document.getElementById("tableetudiant");
+            if (tableElement) {
+                tableElement.innerHTML = "Aucune donnée d'étudiant disponible.";
+            }
+        }
+    });
